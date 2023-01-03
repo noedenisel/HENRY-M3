7d00b1cbdb812c684da1efcec1961840cc91b7e3
Noe Denise <noelialombardo@hotmail.com>
2023-01-03 18:54:03 -0300

    Finished WebServer Homework

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -22,3 +22,60 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+
+//1- crear el server
+http
+  .createServer((req, res)=>{
+    //2- crear la ruta /api
+    if(req.url === "/api"){
+      return res
+        .writeHead(200, {"Content-type": "application/json"}) //devolvemos un formato JSON
+        .end(JSON.stringify(beatles))// hay que parsear el JSON a JS
+    }
+
+    //3- crear la ruta /api/Jhon Lennon
+    //console.log(req.url);
+    const beatleName = req.url.split("/").pop().replace("%20", " ") //para sacar el / y el %20
+    //console.log(beatleName);
+    if (req.url.includes("/api/") && beatleName) {
+    const beatle = beatles.filter((integrante) => integrante.name === beatleName);
+    if (!beatle.length) {
+    return res
+      .writeHead(404, { "Content-type": "text/plain" })
+      .end("Beatle not found");
+  }
+  
+  return res
+    .writeHead(200, { "Content-type": "application/json" })
+    .end(JSON.stringify(beatle));
+    }
+
+    //4- conectar html index
+    if(req.url === "/"){
+      const index = fs.readFileSync("./index.html")
+      return res
+        .writeHead(200, {"Content-type" : "text/html"})
+        .end(index)
+    }
+
+    //5- conectar html beatle
+    const beatleOne = req.url.split("/")
+    if (beatleOne.length < 3) {
+      const beatleName = beatleOne[1].replace("%20", " ");
+      // console.log(beatleName);
+      const beatle = beatles.filter((integrante) => integrante.name === beatleName)[0];
+      //console.log(beatle);
+      let template = fs.readFileSync("./beatle.html", "utf-8")
+      template = template.replace("{nombre}", beatle.name)
+      template = template.replace("{nacimiento}", beatle.birthdate)
+      template = template.replace("{imagen}", beatle.profilePic)
+
+      return res
+        .writeHead(200, {"Content-type" : "text/html"})
+        .end(template)
+    }
+
+
+})
+.listen(3001, "localhost")
